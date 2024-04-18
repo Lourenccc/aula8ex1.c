8ec762df92aa69c398cb6cbbaff7390865b319c0
Lourenccc <167427744+Lourenccc@users.noreply.github.com>
2024-04-18 08:08:41 -0300

    Create exercicio1.c

---
+++ exercicio1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int contador;
+    contador = 0;
+
+    while (contador <= 100){
+        printf("contagem %i\n", contador);
+        contador++;
+    }
+
+return 0;
+}
